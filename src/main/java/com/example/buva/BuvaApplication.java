1a1b46ca02c7842e6930f2b45b86497860524310
Hoseok Seong <shsshs0125@naver.com>
2023-07-06 14:12:53 +0900

    init

---
+++ src/main/java/com/example/buva/BuvaApplication.java
@@ -0,0 +1,13 @@
+package com.example.buva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuvaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuvaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/buva/BuvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuvaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
