b716dced4d134bb08c963e631c4f676453d909a4
Hoseok Seong <shsshs0125@naver.com>
2023-07-11 23:07:30 +0900

    Feat: 신고 데이터베이스 insert 기능 완료

--- src/main/java/com/example/buva/report/controller/ReportController.java
+++ src/main/java/com/example/buva/report/controller/ReportController.java
@@ -1,7 +1,25 @@
 package com.example.buva.report.controller;
 
+import com.example.buva.global.security.MyUserDetails;
+import com.example.buva.report.dto.ReportInsertReq;
+import com.example.buva.report.service.ReportService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class ReportController {
+
+    private final ReportService reportService;
+
+    @PostMapping("/reports")
+    public ResponseEntity<?> insertReport(@AuthenticationPrincipal MyUserDetails myUserDetails,
+                                          @RequestBody @Valid ReportInsertReq reportInsertReq) {
+        return reportService.insertReport(myUserDetails, reportInsertReq);
+    }
 }
--- src/main/java/com/example/buva/report/service/ReportService.java
+++ src/main/java/com/example/buva/report/service/ReportService.java
@@ -1,4 +1,33 @@
 package com.example.buva.report.service;
 
+import com.example.buva.global.security.MyUserDetails;
+import com.example.buva.report.dto.ReportInsertReq;
+import com.example.buva.report.dto.ReportInsertResp;
+import com.example.buva.report.entity.Report;
+import com.example.buva.report.repository.ReportRepository;
+import com.example.buva.user.entity.User;
+import com.example.buva.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class ReportService {
+
+    private final ReportRepository reportRepository;
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public ResponseEntity<?> insertReport(MyUserDetails myUserDetails,
+                                          ReportInsertReq reportInsertReq) {
+        User user = userRepository.findById(myUserDetails.user().getId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저가 없습니다"));
+
+        Report report = reportRepository.save(reportInsertReq.toEntity());
+
+        return ResponseEntity.ok().body(new ReportInsertResp(report));
+    }
 }
