24ff847d68763d1442f4290ed3d121bac9d94406
Hoseok Seong <shsshs0125@naver.com>
2023-07-16 16:02:04 +0900

    Feat: 신고 메세지 조회 기능 완료

---
+++ src/main/java/com/example/buva/message/dto/MessageFindResp.java
@@ -0,0 +1,7 @@
+package com.example.buva.message.dto;
+
+import com.example.buva.message.entity.Message;
+
+import java.util.List;
+
+public record MessageFindResp(List<Message> entity) {}
--- src/main/java/com/example/buva/message/repository/MessageRepository.java
+++ src/main/java/com/example/buva/message/repository/MessageRepository.java
@@ -3,5 +3,8 @@ package com.example.buva.message.repository;
 import com.example.buva.message.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findByUsername(String username);
 }
